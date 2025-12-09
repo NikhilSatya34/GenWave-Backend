acf583e1dc1d43335fcc0a55b3d0735de7fc3faf
Nikhil Satya <bnikhilsatya@gmail.com>
2025-12-09 17:50:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ app.use(express.json());
 app.use(cors());
 
 // CONNECT TO MONGODB (Render uses environment variable)
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connected Successfully"))
-    .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Connected Successfully"))
+.catch(err => console.log("Mongo Error:", err));
 
 
 // ===================== CONTACT FORM =====================
